0a9dfda081b85c7e7646b9f9afabbc8f7fc2f27a
Daferdial <daferdial@gmail.com>
2025-02-13 12:56:29 -0600

    html diseño

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone  # Importar timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    # Filtrar posts con fecha de publicación menor o igual a la fecha y hora actual
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
